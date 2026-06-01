0e5f27898f14cf07f8c01f8fd527037f83b2cc43
jonpalencia <jonpalencia20@gmail.com>
2026-06-02 04:54:48 +0800

    add: UserInput.jsx component

---
+++ src/components/UserInput.jsx
@@ -0,0 +1,9 @@
+export default function () {
+  return (
+    <section id="user-input">
+      <div className="input-group">
+        <label htmlFor="test"></label>
+      </div>
+    </section>
+  );
+}
