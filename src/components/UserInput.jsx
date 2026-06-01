95b12436ad6c79d7b41811a584de0b68a4e20068
jonpalencia <jonpalencia20@gmail.com>
2026-06-02 05:20:48 +0800

    refactor: start developing the UserInput.jsx component

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,8 +1,56 @@
+import { useState } from 'react';
+
+// This function expects a JS object as an argument
+// The object should contain the following properties
+// - initialInvestment: The initial investment amount
+// - annualInvestment: The amount invested every year
+// - expectedReturn: The expected (annual) rate of return
+// - duration: The investment duration (time frame)
+
+const initialInput = {
+  initialInput: 1000,
+  annualInvestment: 1200,
+  expectedReturn: 6,
+  duration: 10,
+};
+
+//! TESTING PHASE
 export default function () {
+  const [userInput, setUserInput] = useState(initialInput);
+  const inputHandler = function (e) {
+    setUserInput(prevInput => {
+      const newInput = e.target.value;
+      console.log(newInput);
+      return newInput;
+    });
+  };
+
   return (
     <section id="user-input">
       <div className="input-group">
-        <label htmlFor="test"></label>
+        <p>
+          <label>Initial Investment</label>
+          <input
+            type="number"
+            value={userInput.initialInput}
+            onChange={inputHandler}
+            required
+          />
+        </p>
+        <p>
+          <label>Annual Investment</label>
+          <input type="number" required />
+        </p>
+      </div>
+      <div className="input-group">
+        <p>
+          <label>Expected Return</label>
+          <input type="number" required />
+        </p>
+        <p>
+          <label>Duration</label>
+          <input type="number" required />
+        </p>
       </div>
     </section>
   );
