c1319cad052394d5e2473ecf69a62d60bdaa80a1
jonpalencia <jonpalencia20@gmail.com>
2026-06-05 04:48:46 +0800

    add(new): create Result.jsx component

---
+++ src/components/Result.jsx
@@ -0,0 +1,70 @@
+import { calculateInvestmentResults, formatter } from '../util/investment';
+
+export default function Result({ resultInput }) {
+  const resultCalculation = calculateInvestmentResults(resultInput);
+  const { duration, initialInvestment } = resultInput;
+  const resultArr = resultCalculation.map(res => {
+    const { year, interest, valueEndOfYear, annualInvestment } = res;
+    return (
+      <tr>
+        <td>{year}</td>
+        <td>{initialInvestment}</td>
+        <td>{formatter.format(valueEndOfYear)}</td>
+        <td>{formatter.format(interest)}</td>
+        <td>{formatter.format(annualInvestment)}</td>
+      </tr>
+    );
+  });
+  return (
+    <>
+      <table id="result">
+        <thead>
+          <tr>
+            <th>Year</th>
+            <th>Investment Value</th>
+            <th>Interest (Year)</th>
+            <th>Total Interest</th>
+            <th>Invested Capital</th>
+          </tr>
+        </thead>
+        <tbody>{resultArr}</tbody>
+      </table>
+    </>
+  );
+}
+
+/* 
+<table>
+  <!-- <thead> groups the entire header section -->
+  <thead>
+    <tr>
+      <!-- <th> defines each individual heading cell -->
+      <th>Product Name</th>
+      <th>Price</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>Apple</td>
+      <td>$1.00</td>
+    </tr>
+  </tbody>
+</table>
+
+
+  initialInvestment,
+  annualInvestment,
+  expectedReturn,
+  duration,
+
+  year, interest,  valueEndOfYear, annualInvestment
+
+
+  export const formatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+  minimumFractionDigits: 0,
+  maximumFractionDigits: 0,
+});
+
+*/
