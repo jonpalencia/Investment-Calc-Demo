c8665c661e9ce9e5a20406910e1748e6b37ff176
jonpalencia <jonpalencia20@gmail.com>
2026-06-06 05:18:24 +0800

    refactor: use Number() for type coercion for the UserInput.jsx component

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -15,7 +15,7 @@ export default function ({ setValue, inputState }) {
       const userInput = e.target.value;
       const targetInput = e.target.name;
       const newInputValue = { ...prevInput };
-      newInputValue[targetInput] = +userInput;
+      newInputValue[targetInput] = Number(userInput);
       const calcResult = calculateInvestmentResults(newInputValue);
       // console.log(newInputValue);
       // console.log(calcResult);
