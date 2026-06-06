2d96d99e9b293a1f592db013c8bdea527c808f8d
jonpalencia <jonpalencia20@gmail.com>
2026-06-06 20:45:09 +0800

    add: conditional Result in App.jsx  of output if duration year less than one.

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,19 @@ import { INITIAL_INPUT } from './util/config';
 
 export default function App() {
   const [userInput, setUserInput] = useState(INITIAL_INPUT);
+  const resultOutput = () => {
+    return userInput.duration < 1 ? (
+      <p className="center">Please provide the year duration of 1 or more</p>
+    ) : (
+      <Result resultInput={userInput} />
+    );
+  };
 
   return (
     <>
       <Header />
       <UserInput setValue={setUserInput} inputState={userInput} />
-      <Result resultInput={userInput} />
+      {resultOutput()}
     </>
   );
 }
